73ed9a6e7c1a140ede194025693d9e2ee5bc01fd
Janosch Moor <janosch.moor@gmail.com>
2023-05-09 10:06:35 +0200

    fist commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo sick!')
+    res.send('Yo sick this is live!')
 })
 app.listen(process.env.PORT || 3000)
